75abd948683ec135a720c39aa564cc962ec22321
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-25 21:54:44 +0800

    Create page.jsx

---
+++ src/app/page.jsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="home grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-20 min-h-screen py-24 px-10">
+      <section className="border-b-[3px] h-60">
+        <h2>Category</h2>
+        <h1>Sample title for the card</h1>
+        <p>This is the paragraph summarizing the content of the blog. Want to see it?</p>
+      </section>
+      <section className="border-b-[3px] h-60">
+        <h2>Category</h2>
+        <h1>Sample title for the card</h1>
+        <p>This is the paragraph summarizing the content of the blog. Want to see it?</p>
+      </section>
+      <section className="border-b-[3px] h-60">
+        <h2>Category</h2>
+        <h1>Sample title for the card</h1>
+        <p>This is the paragraph summarizing the content of the blog. Want to see it?</p>
+      </section>
+      <section className="border-b-[3px] h-60">
+        <h2>Category</h2>
+        <h1>Sample title for the card</h1>
+        <p>This is the paragraph summarizing the content of the blog. Want to see it?</p>
+      </section>
+      <section className="border-b-[3px] h-60">
+        <h2>Category</h2>
+        <h1>Sample title for the card</h1>
+        <p>This is the paragraph summarizing the content of the blog. Want to see it?</p>
+      </section>
+      <section className="border-b-[3px] h-60">
+        <h2>Category</h2>
+        <h1>Sample title for the card</h1>
+        <p>This is the paragraph summarizing the content of the blog. Want to see it?</p>
+      </section>
+    </main>
+  );
+}
