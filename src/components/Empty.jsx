5fdf70b1d24fdb44adc115d6d3012d40acf19b68
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-02 06:56:46 +0800

    @updated Font size 26px to 22px

--- src/components/Empty.jsx
+++ src/components/Empty.jsx
@@ -1,7 +1,7 @@
 export default function Empty({ children }) {
   return (
     <section className="absolute left-20 z-2">
-      <h1 className="text-[26px] mb-10">{children}</h1>
+      <h1 className="text-[22px] mb-10">{children}</h1>
     </section>
   );
 }
