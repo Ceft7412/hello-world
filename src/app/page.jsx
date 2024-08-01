d22ce4c2669f9184119cbc2f51366c62b417a83b
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-02 04:35:05 +0800

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,29 @@
-// app/page.js
-import { fetchBlogs } from "@/services/blogService";
 import MainLayout from "./layouts/MainLayout";
-import Loader from "@/components/Loader";
-
 import Card from "@/components/Card";
+import { fetchCategories } from "@/services/category";
+import { fetchBlogs } from "@/services/blogService";
+import { fetchSubcategoriesByCategory } from "@/services/subcategory";
+import Search from "@/components/Search";
+import Filter from "@/components/Filter";
+
 export default async function Home() {
-  let blogs = [];
+  let categories = [];
   try {
-    blogs = await fetchBlogs();
+    categories = await fetchCategories();
   } catch (error) {
-    console.error("Error fetching blogs:", error);
+    console.error("Error fetching categories:", error);
   }
+
+ 
 
   return (
     <MainLayout>
+      <div className="flex justify-between w-full">
+        <Filter categories={categories} />
+        <Search />
+      </div>
       <div className="home self-start grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-20">
-        <Card />
+        <Card  />
       </div>
     </MainLayout>
   );
