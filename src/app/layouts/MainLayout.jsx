c4de5a5f0ff47565b208545cb9d1f20357b822c6
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-02 04:34:58 +0800

    Update MainLayout.jsx

--- src/app/layouts/MainLayout.jsx
+++ src/app/layouts/MainLayout.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -5,7 +6,7 @@ function MainLayout({ children }) {
   return (
     <>
       <Navbar />
-      <main className="flex items-center  justify-center min-h-screen py-24 px-10">
+      <main className="flex flex-col gap-5  items-center  min-h-screen py-24 px-10">
         {children}
       </main>
       <Footer />
