60a326e36653da93bd820a31700c16e7d71d193b
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-02 04:35:32 +0800

    Create category.js

---
+++ src/services/category.js
@@ -0,0 +1,13 @@
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/firebase/firebase";
+
+export const fetchCategories = async () => {
+  const categories = collection(db, "categories");
+  const categoryDocuments = await getDocs(categories);
+  const categoryList = categoryDocuments.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  return categoryList;
+};
+    
