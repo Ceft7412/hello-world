40acd10338e240194f82d82cdfc1b6bfcbfcd659
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-02 04:35:07 +0800

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,33 +1,43 @@
-import React from "react";
+"use client";
 import { fetchBlogs } from "@/services/blogService";
 import Category from "@/components/Category";
 import Empty from "./Empty";
+import CardContainer from "./CardContainer";
+import { useEffect, useState } from "react";
 
-export default async function Card() {
-  let blogs = [];
-  try {
-    blogs = await fetchBlogs();
-  } catch (error) {
-    console.error("Error fetching blogs:", error);
-  }
+export default function Card({ darkTheme }) {
+  const [blogs, setBlogs] = useState([]);
+  const [arg, setArg] = useState("nextjs");
+  useEffect(() => {
+    async function getBlogs() {
+      try {
+        const blogs = await fetchBlogs();
+        setBlogs(blogs);
+      } catch (error) {
+        console.error("Error fetching blogs:", error);
+      }
+    }
+
+    getBlogs();
+  }, [blogs, arg]);
+
+  const filteredBlogs = blogs.filter((blog) =>
+    blog.subcategories.some((subcategory) => subcategory.id === arg)
+  );
 
   return (
     <>
-      {blogs.length === 0 ? (
-        <Empty>No blog posts yet available.</Empty>
-      ) : (
-        blogs.map((blog) => (
-          <section key={blog.id} className="border-b-[3px] h-60">
-            <div className="flex gap-5">
-              {blog.category.map((cat, index) => (
-                <Category key={index}>{cat}</Category>
-              ))}
-            </div>
-            <h1 className="mb-5 text-2xl">{blog.title}</h1>
-            <p className="h-17 line-clamp-2">{blog.summary}</p>
-          </section>
-        ))
-      )}
+      {filteredBlogs.map((blog) => (
+        <CardContainer key={blog.id}>
+          <div className="flex gap-5">
+            {blog.subcategories.map((cat) => (
+              <Category key={cat.id}>{cat.name}</Category>
+            ))}
+          </div>
+          <h1 className="mb-5 text-2xl">{blog.title}</h1>
+          <p className="h-17 line-clamp-2">{blog.summary}</p>
+        </CardContainer>
+      ))}
     </>
   );
 }
