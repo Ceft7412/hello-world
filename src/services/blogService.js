c4decd50f2ce7f9f2ce34a272f68431acafebbc1
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-02 06:57:40 +0800

    Update blogService.js

--- src/services/blogService.js
+++ src/services/blogService.js
@@ -1,11 +1,15 @@
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "@/firebase/firebase";
 
-export const fetchBlogs = async (subcategoryId = null) => {
+export const fetchBlogs = async (subcategoryId = null, searchQuery = "") => {
+  console.log(searchQuery);
   const blogQuery = collection(db, "blogs");
   const querySnapshot = await getDocs(blogQuery);
   let blogs = [];
   querySnapshot.forEach((doc) => {
+    const blog = { id: doc.id, ...doc.data() };
+    console.log("Blog title:", blog.title);
+    console.log("Blog content:", blog.summary);
     blogs.push({ id: doc.id, ...doc.data() });
   });
 
@@ -13,6 +17,18 @@ export const fetchBlogs = async (subcategoryId = null) => {
     blogs = blogs.filter((blog) =>
       blog.subcategories.some((subcategory) => subcategory.id === subcategoryId)
     );
+  }
+
+  if (searchQuery) {
+    blogs = blogs.filter((blog) => {
+      const query = searchQuery.toLowerCase();
+      const titleMatch = blog.title?.toLowerCase().includes(query);
+      console.log(titleMatch);
+      const contentMatch = blog.summary?.toLowerCase().includes(query);
+      console.log(contentMatch);
+      return titleMatch || contentMatch;
+    });
+    console.log("Filtered Blogs:", blogs);
   }
 
   return blogs;
