820fe823be27a0f20db462e6c6db8b144c6c0ff4
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-02 17:26:19 +0800

    Update CardContainer.jsx

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -6,10 +6,10 @@ function CardContainer({ children }) {
   const darkTheme = useSelector((state) => state.theme.darkTheme);
   return (
     <section
-      className={`border-b-[3px] h-60 cursor-pointer hover:scale-105
-   transition-transform duration-[220ms] px-6 py-2 hover:shadow-xl hover:rounded-md ${
+      className={`border-b-[3px] h-60  hover:scale-105
+   transition-transform duration-[220ms] px-2 sm:px-6 py-2 hover:shadow-xl hover:rounded-md ${
      darkTheme ? "hover:bg-gray-950 border-gray-700" : "border-gray-200"
-   }`}
+   } overflow-y-auto`}
     >
       {children}
     </section>
