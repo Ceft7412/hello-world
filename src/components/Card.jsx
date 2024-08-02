f52fd57245ae4ace3704167fafb78fa14a266ba0
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-02 17:26:17 +0800

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
 "use client";
-import { fetchBlogs } from "@/services/blogService";
+import Link from "next/link";
 import Category from "@/components/Category";
 import Empty from "./Empty";
 import CardContainer from "./CardContainer";
-import { useEffect, useState } from "react";
+import { slugify } from "@/utils/slugify";
 
 export default function Card({ darkTheme, blogs }) {
   if (!blogs.length) {
@@ -20,6 +20,14 @@ export default function Card({ darkTheme, blogs }) {
           </div>
           <h1 className="mb-5 text-2xl">{blog.title}</h1>
           <p className="h-17 line-clamp-2">{blog.summary}</p>
+          <Link href={`/blog/${slugify(blog.title)}`}>
+            <button
+              type="button"
+              className="py-2 mt-2 text-blue-600 font-medium underline "
+            >
+              Read more
+            </button>
+          </Link>
         </CardContainer>
       ))}
     </>
