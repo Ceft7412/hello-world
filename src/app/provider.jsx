6751f8caef11ed3ae6d20001d2149fecf0245c9f
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-02 21:47:33 +0800

    @update to save the theme from local storage before component renders

--- src/app/provider.jsx
+++ src/app/provider.jsx
@@ -1,16 +1,19 @@
 // providers.js
 "use client";
 
+import React from "react";
 import { Provider } from "react-redux";
-import Mounth from "./mount";
 import store from "@/redux/store";
+import { setTheme } from "@/redux/themeSlice";
+
+// Load the theme before the application starts
+const savedDarkTheme = JSON.parse(localStorage.getItem("darkTheme"));
+if (savedDarkTheme !== null) {
+  store.dispatch(setTheme(savedDarkTheme));
+}
 
 const Providers = ({ children }) => {
-  return (
-    <Provider store={store}>
-      <Mounth>{children}</Mounth>
-    </Provider>
-  );
+  return <Provider store={store}>{children}</Provider>;
 };
 
 export default Providers;
