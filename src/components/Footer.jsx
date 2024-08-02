6efa2228a18d850c47489ae1611d082f9be7bdae
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-02 17:26:24 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="w-full footer h-40 text-white bg-gray-950">
-      <div className="p-6 px-32 w-full h-full footer__flex flex justify-between items-center">
+    <footer className="w-full footer h-30 sm:h-40 text-white bg-gray-950 min-w-80">
+      <div className="p-6 sm:px-32 w-full h-full footer__flex flex flex-col sm:flex-row justify-between items-center">
         <div className="flex gap-5">
           <a href="https://www.linkedin.com/in/cedrick-caceres-22b8612b4/">
             <img
