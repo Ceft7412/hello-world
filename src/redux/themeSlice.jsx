0f2239a4f9979906834b24dfaa730a75e232d67d
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-02 21:48:28 +0800

    @added setTheme to update with new value not just by toggling

--- src/redux/themeSlice.jsx
+++ src/redux/themeSlice.jsx
@@ -2,17 +2,18 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const themeSlice = createSlice({
   name: "theme",
-
   initialState: {
     darkTheme: false,
   },
-
   reducers: {
     toggleTheme: (state) => {
       state.darkTheme = !state.darkTheme;
     },
+    setTheme: (state, action) => {
+      state.darkTheme = action.payload;
+    },
   },
 });
 
-export const { toggleTheme } = themeSlice.actions;
+export const { toggleTheme, setTheme } = themeSlice.actions;
 export default themeSlice.reducer;
