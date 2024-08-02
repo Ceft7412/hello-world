eb7371000b3bb628bcce1c74071dc5828ec23e61
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-02 17:26:12 +0800

    Update MainLayout.jsx

--- src/app/layouts/MainLayout.jsx
+++ src/app/layouts/MainLayout.jsx
@@ -6,7 +6,7 @@ function MainLayout({ children }) {
   return (
     <>
       <Navbar />
-      <main className="flex flex-col gap-5  items-center  min-h-screen py-24 px-10">
+      <main className="flex flex-col gap-5  items-center  min-h-screen py-16  sm:py-24 px-2 sm:px-10 min-w-80">
         {children}
       </main>
       <Footer />
