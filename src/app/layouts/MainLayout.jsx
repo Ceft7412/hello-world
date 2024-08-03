693297a86c9237953aaaf0ca311c8c9b025d058e
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-03 21:50:56 +0800

    @removed spaces

--- src/app/layouts/MainLayout.jsx
+++ src/app/layouts/MainLayout.jsx
@@ -38,7 +38,7 @@ function MainLayout({ children }) {
           setIsUser={setIsUser}
           setLogoutMessage={setLogoutMessage}
         />
-        <main className="flex flex-col gap-5  items-center  min-h-screen py-16  sm:py-24 px-2 sm:px-10 min-w-80">
+        <main className="flex flex-col gap-5 items-center min-h-screen py-16 sm:py-24 px-2 sm:px-10 min-w-80">
           {children}
         </main>
         <Footer />
