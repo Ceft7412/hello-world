17ce22595eff043e8e16e40d065d885ce7b56ab9
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-03 18:44:57 +0800

    @updated dateObject to store the variable individually

--- src/app/blog/[title]/page.jsx
+++ src/app/blog/[title]/page.jsx
@@ -3,7 +3,8 @@ import MainLayout from "../../layouts/MainLayout";
 import { slugify } from "@/utils/slugify";
 import BlogNavigation from "@/components/BlogNavigation";
 import { JSDOM } from "jsdom";
-
+import Like from "@/components/Like";
+import LikesCount from "@/components/LikesCount";
 export default async function BlogPost({ params }) {
   const { title } = params;
   let blogs = [];
@@ -43,6 +44,12 @@ export default async function BlogPost({ params }) {
     tagName: heading.tagName.toLowerCase(),
     text: heading.textContent,
   }));
+  console.log(blog);
+
+  const dateObj = blog.date.toDate();
+  const month = dateObj.toLocaleString("default", { month: "long" });
+  const day = dateObj.getDate();
+  const year = dateObj.getFullYear();
 
   return (
     <MainLayout>
@@ -51,12 +58,18 @@ export default async function BlogPost({ params }) {
           <article className="">
             <header className="border-b">
               <h1 className="text-4xl font-bold">{blog.title}</h1>
-              <div className="my-4">
-                {blog.subcategories.map((cat) => (
-                  <span key={cat.id} className="mr-2 text-gray-500">
-                    {cat.name}
-                  </span>
-                ))}
+              <div className="my-4 flex flex-col">
+                <span className="">By {blog.author}</span>
+                <span>
+                  {month} {day} {year}
+                </span>
+                <div className="mt-2">
+                  {blog.subcategories.map((cat) => (
+                    <span key={cat.id} className="mr-2 text-gray-500">
+                      {cat.name}
+                    </span>
+                  ))}
+                </div>
               </div>
             </header>
             <section
@@ -70,6 +83,13 @@ export default async function BlogPost({ params }) {
         <aside className="fixed hidden xl:block right-10 w-[350px] ml-8 h-[300px]">
           <BlogNavigation headings={navHeadings} />
         </aside>
+      </div>
+      <div className="flex flex-col">
+        <div className="flex flex-col items-center">
+          <Like blog={blog} />
+          <LikesCount blog={blog} />
+        </div>
+        <div className=""></div>
       </div>
     </MainLayout>
   );
