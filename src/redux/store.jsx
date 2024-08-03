a2c0e73ebd4c9a5d16ffaa2677578a4c998f66a2
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-03 15:13:51 +0800

    @added Modal Reducer

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -2,10 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "../redux/themeSlice";
 import authReducer from "../redux/authSlice";
 import blogsReducer from "../redux/blogsSlice";
+import modalReducer from "../redux/modalSlice";
+
 export default configureStore({
   reducer: {
     theme: themeReducer,
     auth: authReducer,
     blogs: blogsReducer,
+    modal: modalReducer,
   },
 });
