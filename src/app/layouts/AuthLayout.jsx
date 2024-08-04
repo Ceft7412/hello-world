ac18aee41d69913ae495fc42500fc498550edc6c
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-04 11:41:32 +0800

    @update Padding bottom adjusted.

--- src/app/layouts/AuthLayout.jsx
+++ src/app/layouts/AuthLayout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function AuthLayout({ children }) {
   return (
-    <main className="flex bg-gray-100 items-center justify-center min-h-screen pb-32 px-10">
+    <main className="flex bg-gray-100 items-center justify-center min-h-screen  px-10">
       {children}
     </main>
   );
