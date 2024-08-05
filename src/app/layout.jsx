c44f972d7c666c6946e64de613174c91937ff1ba
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-05 11:52:58 +0800

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import "@/resources/sass/styles.scss";
 import NextTopLoader from "nextjs-toploader";
 import Providers from "./provider";
+import ThemeProvider from "@/components/Provider/ThemeProvider";
 
 export const metadata = {
   title: "Hello World",
@@ -8,7 +9,6 @@ export const metadata = {
     "Hello World is a blogging platform built with the intention of sharing knowledge and ideas.",
   keywords: "next.js, react, javascript, web development, blog",
   author: "Cedrick Caceres",
-  viewport: "width=device-width, initial-scale=1",
   charset: "UTF-8",
   "og:title": "Hello World",
   "og:description":
@@ -16,12 +16,18 @@ export const metadata = {
   "og:url": "/",
 };
 
+export const viewport = "width=device-width, initial-scale=1";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <NextTopLoader color="purple" />
-        <Providers>{children}</Providers>
+        <Providers>
+          <ThemeProvider>
+            <NextTopLoader color="purple" />
+            {children}
+          </ThemeProvider>
+        </Providers>
       </body>
     </html>
   );
