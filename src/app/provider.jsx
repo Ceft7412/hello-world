783017c626691946fbec71fc8dea59bddd2bdbdb
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-05 11:52:59 +0800

    Update provider.jsx

--- src/app/provider.jsx
+++ src/app/provider.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import store from "@/redux/store";
+
 const Providers = ({ children }) => {
   return <Provider store={store}>{children}</Provider>;
 };
