3e8edd6fa01047194a9ae14828d37b9fb8735ceb
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-06 08:00:54 +0800

    @create An all blogs page, displaying the created blogs of the user.

---
+++ src/app/admin/all-blogs/page.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+import AdminLayout from "@/app/layouts/AdminLayout";
+
+export default function page() {
+  return (
+    <AdminLayout>
+      <div>page</div>
+    </AdminLayout>
+  );
+}
