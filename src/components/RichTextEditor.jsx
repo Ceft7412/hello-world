9b155a20de33c835ddf0777f1e4fd54d0747bc16
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-06 08:04:43 +0800

    @add Text editor component to hold the editor.

---
+++ src/components/RichTextEditor.jsx
@@ -0,0 +1,29 @@
+// components/RichTextEditor.js
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+const RichTextEditor = ({ value, onChange }) => {
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ["bold", "italic", "underline"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["clean"],
+    ],
+  };
+
+  const formats = ["header", "bold", "italic", "underline", "list", "bullet"];
+
+  return (
+    <ReactQuill
+      value={value}
+      onChange={onChange}
+      modules={modules}
+      formats={formats}
+      theme="snow"
+    />
+  );
+};
+
+export default RichTextEditor;
