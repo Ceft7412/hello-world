951d9cd1a2bd2ab31ec15bfd2f1751d479625556
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-07 16:29:42 +0800

    @conditional Render black bg when themeColor is dark.

--- src/components/Modals/ModalCenter.jsx
+++ src/components/Modals/ModalCenter.jsx
@@ -1,12 +1,18 @@
 "use client";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const ModalCenter = ({ isOpen, onClose, onConfirm, title, message }) => {
+  const themeColor = useSelector((state) => state.theme.themeColor);
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-      <div className="bg-white p-6 rounded-md shadow-md w-11/12 max-w-md">
+      <div
+        className={`${
+          themeColor === "dark" ? "bg-gray-900" : "bg-white"
+        } p-6 rounded-md shadow-md w-11/12 max-w-md`}
+      >
         <h2 className="text-lg font-semibold mb-4">{title}</h2>
         <p className="mb-6">{message}</p>
         <div className="flex justify-end">
