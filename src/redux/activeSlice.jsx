6a76cc5b49a5c5f4bf4365e604ecac374127acc8
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-07 16:33:04 +0800

    @update Check first if the window is not undefined before we access it local storage to avoid error.

--- src/redux/activeSlice.jsx
+++ src/redux/activeSlice.jsx
@@ -1,13 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+let initialState;
+if (typeof window !== "undefined") {
+  initialState = localStorage.getItem("active")
+    ? JSON.parse(localStorage.getItem("active"))
+    : { active: "all-blogs" };
+} else {
+  initialState = { active: "all-blogs" };
+}
+
 export const activeSlice = createSlice({
   name: "active",
-  initialState: {
-    active: "all-blogs",
-  },
+  initialState,
   reducers: {
     setActive: (state, action) => {
       state.active = action.payload;
+      if (typeof window !== "undefined") {
+        localStorage.setItem("active", JSON.stringify(state));
+      }
     },
   },
 });
