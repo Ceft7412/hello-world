88785b4652c5b565b108f14cb464981c4d341dc7
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-07 17:42:39 +0800

    @update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setActive } from "@/redux/activeSlice";
-import { useRouter, usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function Sidebar({ currentRoute }) {
   const router = useRouter();
